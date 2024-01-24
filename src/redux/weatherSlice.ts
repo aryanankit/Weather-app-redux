93e66da6aeeccbd363f8f5cc6059532c879bdba7
aryanankit <a.arya8279@gmail.com>
2024-01-24 22:09:15 +0530

    Some changes Weather Slice

--- src/redux/weatherSlice.ts
+++ src/redux/weatherSlice.ts
@@ -16,7 +16,7 @@ const initialState: WeatherState = {
 export const fetchWeather = createAsyncThunk(
   'weather/fetchWeather', async (city: string) => {
   const response = await axios.get(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_KEY}/`);
+    `https://api.openweathermap.org/data/2.5/weather?q=${city&lat=&long=}&appid=${process.env.REACT_APP_KEY}/`);
   return response.data;
 });
 
